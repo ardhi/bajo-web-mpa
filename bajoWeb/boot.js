d4458ea3db4c33e00f6c012810e3be7772ef43c5
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-23 08:57:11 +0700

    Refactor

--- bajoWeb/boot.js
+++ bajoWeb/boot.js
@@ -4,24 +4,27 @@ import buildRoutes from '../lib/build-routes.js'
 import notFound from '../lib/not-found.js'
 import error from '../lib/error.js'
 
-async function boot () {
-  const { getConfig, importPkg, importModule } = this.bajo.helper
-  const bodyParser = await importPkg('bajo-web:@fastify/formbody')
-  const cfg = getConfig('bajoWebMpa')
-  const prefix = cfg.prefix
-  const cfgWeb = getConfig('bajoWeb', { full: true })
-  const routeHook = await importModule(`${cfgWeb.dir}/lib/route-hook.js`)
-
-  await this.bajoWeb.instance.register(async (ctx) => {
-    this.bajoWebMpa.instance = ctx
-    await routeHook.call(this, ctx, 'bajoWebMpa')
-    await ctx.register(bodyParser)
-    await error.call(this, ctx)
-    await viewEngine.call(this, ctx)
-    await decorate.call(this, ctx)
-    await buildRoutes.call(this, ctx)
-    await notFound.call(this, ctx)
-  }, { prefix })
+const boot = {
+  level: 10,
+  handler: async function () {
+    const { getConfig, importPkg, importModule, runHook } = this.bajo.helper
+    const bodyParser = await importPkg('bajo-web:@fastify/formbody')
+    const cfg = getConfig('bajoWebMpa')
+    const prefix = cfg.prefix
+    const cfgWeb = getConfig('bajoWeb', { full: true })
+    const routeHook = await importModule(`${cfgWeb.dir}/lib/route-hook.js`)
+    await this.bajoWeb.instance.register(async (ctx) => {
+      this.bajoWebMpa.instance = ctx
+      await runHook('bajoWebMpa:afterCreateContext', ctx)
+      await routeHook.call(this, 'bajoWebMpa')
+      await ctx.register(bodyParser)
+      await error.call(this, ctx)
+      await viewEngine.call(this, ctx)
+      await decorate.call(this, ctx)
+      await buildRoutes.call(this, ctx)
+      await notFound.call(this, ctx)
+    }, { prefix })
+  }
 }
 
 export default boot
---
+++ bajoWebMpa/global/route-path.js
@@ -0,0 +1,5 @@
+function routePath (env, name) {
+  return this.bajoWeb.helper.routePath(name)
+}
+
+export default routePath
