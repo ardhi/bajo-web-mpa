a9bc051073e9cb52a4282f599255e82bac346bac
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-29 20:42:50 +0700

    Refactor

--- bajoWebMpa/extension/tag.js
+++ bajoWebMpa/extension/tag.js
@@ -1,5 +1,5 @@
 import format from '../../lib/tag/format.js'
-import getAttr from '../../lib/tag/get-attr.js'
+import defGetAttr from '../../lib/tag/get-attr.js'
 
 class Tag {
   constructor (options) {
@@ -19,15 +19,17 @@ class Tag {
 
   run (context, ...args) {
     // const callback = args.pop()
-    const { kebabCase, fs } = this.scope.bajoWebMpa.util
+    const { kebabCase, fs, get } = this.scope.bajoWebMpa.util
     const { resolveTagPath } = this.scope.bajoWebMpa.helper
     const { getConfig } = this.scope.bajo.helper
     const cfg = getConfig('bajoWebMpa')
     const body = args.pop()
     const name = kebabCase(args.shift())
-    const { attr, attributes, params } = getAttr.call(this, { name, context, args })
     const file = resolveTagPath(name, context.ctx.theme, true)
     const { theme, themes } = context.ctx
+    let getAttr = get(this.scope, `${theme.plugin}.helper.getAttr.exec`)
+    if (!getAttr) getAttr = defGetAttr
+    const { attr, attributes, params } = getAttr.call(this, theme.name, { name, context, args })
     const locals = { tag: name, params, attr, attributes, content: body(), theme, themes }
     const fragment = fs.readFileSync(file, 'utf8').replaceAll('\r', '') // TODO: replace new line inside the brackets only
     const ret = context.env.renderString(fragment, locals)
