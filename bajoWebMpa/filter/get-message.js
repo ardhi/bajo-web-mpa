0d42904dc2384c03b4acd230407a5072542d9dd3
Ardhi Lukianto <ardhi@lukianto.com>
2023-09-14 19:57:57 +0700

    Bug fix

--- bajoWebMpa/filter/get-message.js
+++ bajoWebMpa/filter/get-message.js
@@ -1,4 +1,4 @@
-function getMessage (env, obj, field) {
+function getMessage (env, obj = {}, field) {
   const { find } = this.bajoWebMpa.util
   const item = find(obj.details ?? [], { field })
   return item ? item.error : undefined
