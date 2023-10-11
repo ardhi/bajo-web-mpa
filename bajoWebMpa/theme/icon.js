5b8d61ce7429a67348a3db38fb378e045feb88cb
Ardhi Lukianto <ardhi@lukianto.com>
2023-10-11 21:16:20 +0700

    Add components & attrs

--- bajoWebMpa/theme/icon.js
+++ bajoWebMpa/theme/icon.js
@@ -14,7 +14,10 @@ const icon = {
   gridSmall: 'bi bi-grid-3x3-gap',
   book: 'bi bi-book',
   arrowLeft: 'bi bi-arrow-left',
-  arrowRight: 'bi bi-arrow-right'
+  arrowRight: 'bi bi-arrow-right',
+  table: 'bi bi-table',
+  caretUp: 'bi bi-caret-up-fill',
+  caretDown: 'bi bi-caret-down-fill'
 }
 
 export default icon
