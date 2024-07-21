16afae5f72d77350e49cadca3597b2478c306bb4
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-21 20:02:17 +0700

    Emoji support

--- bajo/method/render.js
+++ bajo/method/render.js
@@ -10,7 +10,7 @@ async function render (name, params, reply) {
   if (!viewEngine) throw this.error('No view engine available')
   let content = await this.app[viewEngine.ns].render(name, params, reply)
   const exts = ['.html', ...viewEngine.fileExts]
-  if (this.config.emoji && exts.includes(ext)) content = emoji.emojify(content)
+  if (this.config.emoji.enabled && exts.includes(ext)) content = emoji.emojify(content)
   if (this.config.prettier.enabled && exts.includes(ext)) content = await prettier.format(content, this.config.prettier.options)
   if (this.config.minify.enabled && ['.js', '.css', ...exts].includes(ext)) content = minify(content, this.config.minify.options)
   return content
